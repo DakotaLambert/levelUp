425692dcce84289d5ed1e20c88fd1e181f9970ef
Dakota Lambert <dakotalambertbiz@gmail.com>
2021-08-03 17:07:16 -0400

    Debugged getGames, and got them rendered to the DOM

--- levelupapi/views/__init__.py
+++ levelupapi/views/__init__.py
@@ -1,2 +1,3 @@
 from .auth import login_user, register_user
-from .gametype import GameTypeView
+from .gametype import GameTypeView, GameTypeSerializer, GameType
+from .game import Game, GameView, GameSerializer
