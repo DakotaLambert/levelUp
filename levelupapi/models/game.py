419cc65c04f598aa1dbe94be9c8311c40f3ec613
Dakota Lambert <dakotalambertbiz@gmail.com>
2021-08-05 11:29:15 -0400

    Added a lot of functionality, oops.

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -17,3 +17,5 @@ class Game(models.Model):
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     maker = models.CharField(max_length=50)
     
+    def __str__(self):
+        return self.name
