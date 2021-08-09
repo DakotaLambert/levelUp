b035a0502441f2c574494d6e8ae6d884c542a7ec
Dakota Lambert <dakotalambertbiz@gmail.com>
2021-08-09 10:23:05 -0400

    Proper handling of joining events from client

--- levelupapi/views/__init__.py
+++ levelupapi/views/__init__.py
@@ -2,3 +2,4 @@ from .auth import login_user, register_user
 from .game_type import GameTypeView, GameTypeSerializer, GameType
 from .game import Game, GameView, GameSerializer
 from .event import EventView
+from .profile import Profile
